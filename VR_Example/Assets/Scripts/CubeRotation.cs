67cacc60e5c9ea5178899291e757d490effbb169
ingsnchz <42521986+ingsnchz@users.noreply.github.com>
2023-09-11 07:59:42 -0600

    clase11

--- VR_Example/Assets/Scripts/CubeRotation.cs
+++ VR_Example/Assets/Scripts/CubeRotation.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CubeRotation : MonoBehaviour
 {
+    //variables --> tipo de dato + nombre + simbolo de asignacion + valor a asignar +;
+    public int edad = 15;
+    public float altura = 1.83f;
+    public string nombre= "Andrea";
+    public char letras = '1';
+    public bool IsDoorOpen = false;
+
+    // Function
+
+    void setNombre()
+    {
+        //Logica
+    }
+
+    void setNombre(string _nombre)
+    {
+        nombre = _nombre;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("hola, esto es una prueba llamada Start()");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("actualiacion de la funcion Update()");
+        transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+        setRotationOfCube();
     }
 }
